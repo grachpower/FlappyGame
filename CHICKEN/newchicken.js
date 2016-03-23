17852aa40c2733edec287f134b6e6fb425449298
Artyom Andreev <amiscary@gmail.com>
2016-03-23 14:03:44 +0300

    v 1.0

---
+++ CHICKEN/newchicken.js
@@ -0,0 +1,512 @@
+var game = new Phaser.Game(600, 600, Phaser.AUTO, '');
+var topScore = 0;
+var topScoreText;
+var lastScore = 0;
+var userName = "GUEST";
+
+//game inside prototype
+playingState = function () {};
+playingState.prototype = {
+
+	preload: function () {
+		this.game.stage.backgroundColor = '#71c5cf';
+		this.game.load.spritesheet('chicken', 'assets/narutomini.png', 56, 54);
+		this.game.load.image('pipe', 'assets/pipe2.png');
+		this.game.load.image('cloud', 'assets/cloudnew.png');
+		this.game.load.image('ground', 'assets/ground.png');
+	},
+
+	create: function () {
+
+		this.score = 0;
+
+		//init chicken
+		this.chicken = this.game.add.sprite(270, 270, 'chicken');
+		//enable chicken gravity
+		this.game.physics.arcade.enable(this.chicken);
+		//set chicken gravity Y
+		this.chicken.body.gravity.y = 1000;
+		//		this.chicken.body.collideWorldBounds = true;
+
+		//set chicken animation
+		this.chicken.animations.add('fly', [1, 2, 3], 5, true);
+		this.chicken.animations.play('fly');
+
+		//make pipe group
+		this.pipes = this.game.add.group();
+		this.pipes.enableBody = true;
+
+		//timer to make ne pipes
+		//this.pipeTimer =
+		this.game.time.events.loop(1500, this.add_row_pipes, this);
+
+		//set score timer
+		//this.scoreTimer =
+		this.game.time.events.loop(500, this.calcScore, this, false);
+
+		//create clouds
+		this.clouds = this.game.add.group();
+		this.clouds.enableBody = true;
+		this.game.time.events.loop(1500, this.startClouds, this, false);
+
+		//create grounds group
+		this.grounds = this.game.add.group();
+		this.grounds.enableBody = true;
+		this.game.time.events.loop(5000, this.startGround, this);
+		//first ground start on start
+		this.ground1 = this.grounds.create(0, 570, 'ground');
+		this.ground1.body.velocity.x = -200;
+		if (this.ground1.inWorld == false) {
+			this.ground1.kill;
+		};
+		//second ground on start
+		this.ground2 = this.grounds.create(600, 570, 'ground');
+		this.ground2.body.velocity.x = -200;
+		if (this.ground2.inWorld == false) {
+			this.ground2.kill;
+		};
+
+		this.scoreText = this.game.add.text(30, 30, 'score: 0', {
+			fontSize: '24px',
+			fill: 'blue'
+		});
+
+		this.jumpkey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.jumpkey.onDown.add(this.jump, this)
+	},
+
+	update: function () {
+		//restart game when drops under||over world
+		if (this.chicken.inWorld == false) {
+			this.restartGame();
+		};
+
+		//jump on Tap
+		if (this.input.activePointer.isDown) {
+			this.jump();
+		};
+
+		//restart game if chicken overlaps the pipe
+		this.game.physics.arcade.overlap(this.chicken, this.pipes, this.restartGame, null, this);
+		this.game.physics.arcade.overlap(this.chicken, this.grounds, this.restartGame, null, this);
+	},
+
+	//create one pipe
+	add_one_pipe: function (x, y) {
+		this.pipe = this.pipes.create(x, y, 'pipe');
+		this.pipe.body.velocity.x = -200;
+		if (this.pipe.inWorld == false) {
+			this.pipe.kill;
+		};
+	},
+
+	//create 6 pipes & hole
+	add_row_pipes: function () {
+		var hole = Math.floor(Math.random() * 5) + 1;
+
+		for (var i = 0; i < 8; i++) {
+			if (i != hole && i !== (hole + 1)) {
+				this.add_one_pipe(600, i * 80);
+			};
+		};
+	},
+
+	startClouds: function () {
+		var rate = Math.floor(Math.random() * 100) + 100;
+		this.cloud = this.clouds.create(600, 15, 'cloud');
+		this.cloud.body.velocity.x = -rate;
+		if (this.cloud.inWorld == false) {
+			this.cloud.kill;
+		};
+	},
+
+	startGround: function () {
+		this.ground = this.grounds.create(600, 570, 'ground');
+		this.ground.body.velocity.x = -200;
+		if (this.ground.inWorld == false) {
+			this.ground.kill;
+		};
+	},
+
+	restartGame: function () {
+		if (this.score > topScore) {
+			topScore = this.score;
+		};
+		lastScore = this.score;
+		this.chicken.animations.stop();
+		this.chicken.frame = 3;
+		this.jump();
+		this.chicken.body.velocity.x = -400;
+		//------------------ajax bad down------------------//
+		//set user paratemers
+		var xhr = new XMLHttpRequest();
+
+		this.userName = userName;
+
+		var requestStr = "/setUser?" + "name=" + this.userName + "&score=" + this.score;
+		console.log(requestStr);
+
+		xhr.open('GET', requestStr, true);
+		// xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+		xhr.onreadystatechange = function() {
+			// console.log(xhr.readyState + " получено символов:" + xhr.responseText);
+		};
+		xhr.send();
+		//------------------ajax bad up------------------//
+		setTimeout(function () {
+			this.game.state.start('mainMenuState')
+		}, 400);
+	},
+
+	//add chicken vertical speed
+	jump: function () {
+		this.chicken.body.velocity.y = -300;
+	},
+
+	//this increment score & show this
+	calcScore: function () {
+		this.score += 10;
+		this.scoreText.text = 'Score: ' + this.score;
+	},
+};
+
+//main menu prototype
+mainMenuState = function () {};
+mainMenuState.prototype = {
+	preload: function () {
+		this.game.stage.backgroundColor = '#71c5cf';
+		this.game.load.spritesheet('chicken', 'assets/narutomini.png', 56, 54);
+		this.game.load.image('cloud', 'assets/cloudnew.png');
+		this.game.load.image('ground', 'assets/ground.png');
+	},
+
+	create: function () {
+		this.chicken = this.game.add.sprite(270, 270, 'chicken');
+		this.chicken.frame = 1;
+
+		topScoreText = this.game.add.text(190, 100, 'Record: ' + topScore, {
+			fontSize: '40px',
+			fontFamily: 'Times',
+			fill: 'blue',
+		});
+
+		if (lastScore > 0) {
+			this.game.add.text(160, 170, 'Your last score: ' + lastScore, {
+				fontSize: '30px',
+				fontFamily: 'Times',
+				fill: 'blue',
+			});
+		};
+
+		this.game.add.text(220, 350, 'Tap to start', {
+			fontSize: '24px',
+			fontFamily: 'Roboto',
+			fill: 'cyan',
+		});
+
+		//create clouds
+		this.clouds = this.game.add.group();
+		this.clouds.enableBody = true;
+		this.game.time.events.loop(1500, this.startClouds, this, false);
+
+		//create ground
+		this.grounds = this.game.add.group();
+		this.grounds.enableBody = true;
+		this.game.time.events.loop(5000, this.startGround, this);
+		//first ground start on start
+		this.ground1 = this.grounds.create(0, 570, 'ground');
+		this.ground1.body.velocity.x = -200;
+		if (this.ground1.inWorld == false) {
+			this.ground1.kill;
+		};
+		//second ground on start
+		this.ground2 = this.grounds.create(600, 570, 'ground');
+		this.ground2.body.velocity.x = -200;
+		if (this.ground2.inWorld == false) {
+			this.ground2.kill;
+		};
+
+		//------------------ajax bad down------------------//
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', '/getUsers', true);
+		xhr.send();
+		var topUsers;
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 3){
+				// console.log(xhr.responseText);
+				topUsers = JSON.parse(xhr.responseText);
+				// topUsers = xhr.responseText;
+				// console.log(topUsers);
+				// console.log(xhr.responseText);
+			}
+		};
+		//------------------ajax bad up------------------//
+		setTimeout(function (){
+			// console.log(topUsers);
+			this.deftextheight = 150;
+			for (var i = 0; i < 10; i++){
+				this.game.add.text(40, this.deftextheight, topUsers[i].user + ": " + topUsers[i].score, {
+					fontSize: '16px',
+					fontFamily: 'Roboto',
+					fill: 'green',
+				});
+				this.deftextheight += 20;
+				if (i == topUsers.length-1) i = 10;
+			};
+		}, 200);
+
+		this.jumpkey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.jumpkey.onDown.add(function () {
+			this.game.state.start('playingState');
+		}, this);
+
+
+	},
+
+	update: function () {
+		if (this.input.activePointer.isDown) {
+			this.game.state.start('playingState');
+		};
+	},
+
+	startClouds: function () {
+		var rate = Math.floor(Math.random() * 100) + 100;
+		this.cloud = this.clouds.create(600, 15, 'cloud');
+		this.cloud.body.velocity.x = -rate;
+		if (this.cloud.inWorld == false) {
+			this.cloud.kill;
+		};
+	},
+
+	startGround: function () {
+		this.ground = this.grounds.create(600, 570, 'ground');
+		this.ground.body.velocity.x = -200;
+		if (this.ground.inWorld == false) {
+			this.ground.kill;
+		};
+	},
+};
+
+startUpState = function (){};
+startUpState.prototype = {
+	preload: function () {
+		this.game.stage.backgroundColor = '#71c5cf';
+		this.game.load.spritesheet('chicken', 'assets/narutomini.png', 56, 54);
+		this.game.load.image('cloud', 'assets/cloudnew.png');
+		this.game.load.image('ground', 'assets/ground.png');
+		this.game.load.image('buttonUp', 'assets/button.png');
+	},
+
+	create: function () {
+
+		this.game.add.text(180, 170, 'TYPE UR NAME', {
+			fontSize: '30px',
+			fontFamily: 'Times',
+			fill: 'blue',
+		});
+
+		this.inputName = this.game.add.text(220, 230, userName, {
+			fontSize: '36px',
+			fontFamily: 'Times',
+			fill: 'RED',
+		});
+
+
+		this.button = this.game.add.button(250, 300, 'buttonUp', this.actionOnClick, this);
+
+		//create clouds
+		this.clouds = this.game.add.group();
+		this.clouds.enableBody = true;
+		this.game.time.events.loop(1500, this.startClouds, this, false);
+
+		//create ground
+		this.grounds = this.game.add.group();
+		this.grounds.enableBody = true;
+		this.game.time.events.loop(5000, this.startGround, this);
+		//first ground start on start
+		this.ground1 = this.grounds.create(0, 570, 'ground');
+		this.ground1.body.velocity.x = -200;
+		if (this.ground1.inWorld == false) {
+			this.ground1.kill;
+		};
+		//second ground on start
+		this.ground2 = this.grounds.create(600, 570, 'ground');
+		this.ground2.body.velocity.x = -200;
+		if (this.ground2.inWorld == false) {
+			this.ground2.kill;
+		};
+
+		//init keyboard
+		this.keyQ = this.game.input.keyboard.addKey(Phaser.Keyboard.Q);
+		this.keyQ.onDown.add(function () {
+			userName += 'Q';
+			this.inputName.text = userName;
+		}, this);
+		this.keyW = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
+		this.keyW.onDown.add(function () {
+			userName += 'W';
+			this.inputName.text = userName;
+		}, this);
+		this.keyE = this.game.input.keyboard.addKey(Phaser.Keyboard.E);
+		this.keyE.onDown.add(function () {
+			userName += 'E';
+			this.inputName.text = userName;
+		}, this);
+		this.keyR = this.game.input.keyboard.addKey(Phaser.Keyboard.R);
+		this.keyR.onDown.add(function () {
+			userName += 'R';
+			this.inputName.text = userName;
+		}, this);
+		this.keyT = this.game.input.keyboard.addKey(Phaser.Keyboard.T);
+		this.keyT.onDown.add(function () {
+			userName += 'T';
+			this.inputName.text = userName;
+		}, this);
+		this.keyY = this.game.input.keyboard.addKey(Phaser.Keyboard.Y);
+		this.keyY.onDown.add(function () {
+			userName += 'Y';
+			this.inputName.text = userName;
+		}, this);
+		this.keyU = this.game.input.keyboard.addKey(Phaser.Keyboard.U);
+		this.keyU.onDown.add(function () {
+			userName += 'U';
+			this.inputName.text = userName;
+		}, this);
+		this.keyI = this.game.input.keyboard.addKey(Phaser.Keyboard.I);
+		this.keyI.onDown.add(function () {
+			userName += 'I';
+			this.inputName.text = userName;
+		}, this);
+		this.keyO = this.game.input.keyboard.addKey(Phaser.Keyboard.O);
+		this.keyO.onDown.add(function () {
+			userName += 'O';
+			this.inputName.text = userName;
+		}, this);
+		this.keyP = this.game.input.keyboard.addKey(Phaser.Keyboard.P);
+		this.keyP.onDown.add(function () {
+			userName += 'P';
+			this.inputName.text = userName;
+		}, this);
+		this.keyA = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
+		this.keyA.onDown.add(function () {
+			userName += 'A';
+			this.inputName.text = userName;
+		}, this);
+		this.keyS = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
+		this.keyS.onDown.add(function () {
+			userName += 'S';
+			this.inputName.text = userName;
+		}, this);
+		this.keyD = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
+		this.keyD.onDown.add(function () {
+			userName += 'D';
+			this.inputName.text = userName;
+		}, this);
+		this.keyF = this.game.input.keyboard.addKey(Phaser.Keyboard.F);
+		this.keyF.onDown.add(function () {
+			userName += 'F';
+			this.inputName.text = userName;
+		}, this);
+		this.keyG = this.game.input.keyboard.addKey(Phaser.Keyboard.G);
+		this.keyG.onDown.add(function () {
+			userName += 'G';
+			this.inputName.text = userName;
+		}, this);
+		this.keyH = this.game.input.keyboard.addKey(Phaser.Keyboard.H);
+		this.keyH.onDown.add(function () {
+			userName += 'H';
+			this.inputName.text = userName;
+		}, this);
+		this.keyJ = this.game.input.keyboard.addKey(Phaser.Keyboard.J);
+		this.keyJ.onDown.add(function () {
+			userName += 'J';
+			this.inputName.text = userName;
+		}, this);
+		this.keyK = this.game.input.keyboard.addKey(Phaser.Keyboard.K);
+		this.keyK.onDown.add(function () {
+			userName += 'K';
+			this.inputName.text = userName;
+		}, this);
+		this.keyL = this.game.input.keyboard.addKey(Phaser.Keyboard.L);
+		this.keyL.onDown.add(function () {
+			userName += 'L';
+			this.inputName.text = userName;
+		}, this);
+		this.keyZ = this.game.input.keyboard.addKey(Phaser.Keyboard.Z);
+		this.keyZ.onDown.add(function () {
+			userName += 'Z';
+			this.inputName.text = userName;
+		}, this);
+		this.keyX = this.game.input.keyboard.addKey(Phaser.Keyboard.X);
+		this.keyX.onDown.add(function () {
+			userName += 'X';
+			this.inputName.text = userName;
+		}, this);
+		this.keyC = this.game.input.keyboard.addKey(Phaser.Keyboard.C);
+		this.keyC.onDown.add(function () {
+			userName += 'C';
+			this.inputName.text = userName;
+		}, this);
+		this.keyV = this.game.input.keyboard.addKey(Phaser.Keyboard.V);
+		this.keyV.onDown.add(function () {
+			userName += 'V';
+			this.inputName.text = userName;
+		}, this);
+		this.keyB = this.game.input.keyboard.addKey(Phaser.Keyboard.B);
+		this.keyB.onDown.add(function () {
+			userName += 'B';
+			this.inputName.text = userName;
+		}, this);
+		this.keyN = this.game.input.keyboard.addKey(Phaser.Keyboard.N);
+		this.keyN.onDown.add(function () {
+			userName += 'N';
+			this.inputName.text = userName;
+		}, this);
+		this.keyM = this.game.input.keyboard.addKey(Phaser.Keyboard.M);
+		this.keyM.onDown.add(function () {
+			userName += 'M';
+			this.inputName.text = userName;
+		}, this);
+		this.keyBackSpace = this.game.input.keyboard.addKey(Phaser.Keyboard.BACKSPACE);
+		this.keyBackSpace.onDown.add(function () {
+			userName = userName.substring(0, userName.length-1);
+			this.inputName.text = userName;
+		}, this);
+
+	},
+
+	update: function () {
+		// if (this.input.activePointer.isDown) {
+		// 	this.game.state.start('mainMenuState');
+		// };
+	},
+
+	startClouds: function () {
+		var rate = Math.floor(Math.random() * 100) + 100;
+		this.cloud = this.clouds.create(600, 15, 'cloud');
+		this.cloud.body.velocity.x = -rate;
+		if (this.cloud.inWorld == false) {
+			this.cloud.kill;
+		};
+	},
+
+	startGround: function () {
+		this.ground = this.grounds.create(600, 570, 'ground');
+		this.ground.body.velocity.x = -200;
+		if (this.ground.inWorld == false) {
+			this.ground.kill;
+		};
+	},
+
+	actionOnClick: function() {
+	// background.visible =! background.visible;
+		this.game.state.start('mainMenuState');
+	}
+
+};
+
+//init states & start first state
+game.state.add('playingState', playingState, false);
+game.state.add('mainMenuState', mainMenuState, false);
+game.state.add('startUpState', startUpState, false);
+game.state.start('startUpState');
---
+++ server.js
@@ -0,0 +1,144 @@
+var http = require('http');
+var url = require('url');
+var static = require('node-static');
+var file = new static.Server('.', {
+    cache: 0
+});
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: 'root',
+    database: 'chickengame'
+});
+connection.connect();
+
+
+function accept(req, res) {
+    // console.log(req.url);
+
+    var urlParsed = url.parse(req.url, true)
+    // console.log(urlParsed);
+
+    //get from client scores & username
+    if (urlParsed.pathname == '/setUser'){
+        res.end(" ");
+        insertUser(urlParsed.query.name, urlParsed.query.score);
+    }
+
+    //send to client top 10 players
+    if (urlParsed.pathname == '/getUsers'){
+        selectUsers();
+        setTimeout(function () {
+            var topten = selectUsers.items;
+            // console.log(topten);
+            for (var i = 0; i < 1; i++){
+                res.write(JSON.stringify(topten));
+            }
+            res.end();
+        }, 90);
+    }
+
+    // if (req.url == '/CHICKEN/index.html') {
+    //     selectUsers();
+    //     file.serve(req, res);
+    // }
+    if (req.url == '/') {
+        selectUsers();
+        req.url = "/CHICKEN/index.html";
+        file.serve(req, res);
+    }
+    if(req.url == '/phaser.js') {
+        req.url = "/CHICKEN/phaser.js";
+        file.serve(req, res);
+    }
+    if(req.url == '/newchicken.js') {
+        req.url = "/CHICKEN/newchicken.js";
+        file.serve(req, res);
+    }
+    if(req.url == "/assets/narutomini.png" || req.url == "/assets/ground.png" || req.url == "/assets/cloudnew.png" || req.url == "/assets/pipe2.png" || req.url == "/assets/button.png") {
+        if (req.url == "/assets/narutomini.png"){
+            req.url = "/CHICKEN/assets/narutomini.png";
+            file.serve(req, res);
+        }
+        if (req.url == "/assets/ground.png"){
+            req.url = "/CHICKEN/assets/ground.png";
+            file.serve(req, res);
+        }
+        if (req.url == "/assets/cloudnew.png"){
+            req.url = "/CHICKEN/assets/cloudnew.png";
+            file.serve(req, res);
+        }
+        if (req.url == "/assets/pipe2.png"){
+            req.url = "/CHICKEN/assets/pipe2.png";
+            file.serve(req, res);
+        }
+        if (req.url == "/assets/button.png"){
+            req.url = "/CHICKEN/assets/button.png";
+            file.serve(req, res);
+        }
+    }
+
+}
+// ------ start server -------
+
+if (!module.parent) {
+    http.createServer(accept).listen(8080);
+} else {
+    exports.accept = accept;
+}
+
+
+function insertUser(name, score){
+    var post = {user: '', score: ''};
+    post.user = name;
+    post.score = score;
+    // console.log(post.user+"  "+post.score)
+    // var allUsers = JSON.parse(selectUsers());
+    var allUsers = selectUsers.items;
+    // console.log(allUsers);
+
+    if (!checkUserInDB(name, score, allUsers)){
+        var query = connection.query('INSERT INTO userscores SET ?', post ,function (err, result) {});
+        // console.log(query.sql);
+    }
+}
+
+function updateUser(name, score){
+    var query = connection.query('UPDATE `chickengame`.`userscores` SET `score`=? WHERE `user`=?', [score, name], function(err, result){});
+    // console.log(query.sql);
+}
+
+function selectUsers(){
+    var resulting;
+    connection.query('SELECT * FROM userscores', function (err, result) {
+        resulting = result;
+    });
+    setTimeout(function () {
+        // console.log(resulting);
+        selectUsers.items = resulting;
+    }, 30);
+}
+selectUsers.items;
+
+function checkUserInDB(name, score, allUsers){
+    // console.log(allUsers);
+    //  console.log(name);
+    var user;
+    for (var i = 0;i < allUsers.length; i++){
+        user = allUsers[i];
+        // console.log(user.user);
+        if (user.user == name && user.score < score){
+            updateUser(name, score);
+            // console.log("true");
+            return true;
+        } else if(user.user == name && user.score >= score){
+            // console.log("true");
+            return true;
+        } else{
+            // console.log("false");
+            // return false;
+        }
+    }
+}
