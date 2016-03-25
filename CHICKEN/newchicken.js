65e4b898bbd8c26500e09ceb4da2aa360da4fdea
Artyom Andreev <amiscary@gmail.com>
2016-03-25 15:01:36 +0300

    v.1.8.2

--- CHICKEN/newchicken.js
+++ CHICKEN/newchicken.js
@@ -212,7 +212,7 @@ mainMenuState.prototype = {
 		});
 
 		//add my references
-		this.game.add.text(256, 505, 'FIND ME ON:', {
+		this.game.add.text(236, 505, 'FOLLOW ME:', {
 			fontSize: '14px',
 			fontFamily: 'Roboto',
 			fill: 'blue',
@@ -339,7 +339,7 @@ startUpState.prototype = {
 		});
 
 		//add my references
-		this.game.add.text(256, 505, 'FIND ME ON:', {
+		this.game.add.text(236, 505, 'FOLLOW ME:', {
 			fontSize: '14px',
 			fontFamily: 'Roboto',
 			fill: 'blue',
